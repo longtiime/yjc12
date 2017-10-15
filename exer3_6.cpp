3807386c8cdc94e8788080d8b3a67ab3bd4c64bf
longtiime <31852089+longtiime@users.noreply.github.com>
2017-10-15 23:28:09 +0800

    Add files via upload

---
+++ exer3_6.cpp
@@ -0,0 +1,16 @@
+#include "iostream.h"
+void main()
+{
+	double r,k,s,l;
+	cin>>r>>k;
+	s=3.14*r*r;
+    l=2*3.14*r;
+	if(k==1)
+		cout<<s<<endl;
+	
+		if(k==2)
+			cout<<l<<endl;
+		
+			if(k==3)
+				cout<<s<<l<<endl;
+}
