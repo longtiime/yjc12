f3cd28b3f5470d1e6e883cc5e4f248a5c3d5d52a
longtiime <31852089+longtiime@users.noreply.github.com>
2017-11-27 00:08:02 +0800

    Add files via upload

---
+++ 6——4.cpp
@@ -0,0 +1,11 @@
+#include <iostream.h>
+void main()
+{
+ 	int i,a[20]={1,1,0};
+ 	for(i=2;i<=19;i++)
+ 		a[i]=a[i-1]+a[i-2];
+ 	cout<<"Fibonacci¨ºy¨¢D¦Ì??¡ã20??¨º?¡êo"<<endl;
+ 	for(i=0;i<=19;i++)
+ 		cout<<a[i]<<" ";
+ 	    cout<<endl;
+}
